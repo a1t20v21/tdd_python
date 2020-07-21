53176dfc563f187781cedff2c77d384fb9d90331
a1t20v21 <a1t20v21@gmail.com>
2020-07-21 22:42:59 +0100

    leap year - main function

--- leap_year.py
+++ leap_year.py
@@ -5,3 +5,10 @@ def leap_year(year):
         if year % 100 != 0:
             return True
     return False
+
+if __name__ == '__main__':
+    year = input('Enter an year: ')
+    if leap_year(int(year)):
+        print('{0} is a leap year'.format(year))
+    else:
+        print('{0} is not a leap year'.format(year))
